6db2649c2f28af48a744fb5b0d93e08a77f26947
Elcid-Chawa <elcidchawa@gmail.com>
2021-04-30 07:46:48 +0100

    updated project

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # mybot
-Test messenger bot
+
+This is a test messenger bot app build with Python - Flask library. It connects to a facebook page via a token and respose to messages sent by clients to the facebook page.
+
+## Setup Requirements
+
+- Python 3.6 or higher
+- Flask
+- pymessanger
+- ptyhon-dotenv
+- [ngrok](https://ngrok.com/download)
+
+## Environment Setup
+
+### Install Libraries
+- Install python libraries to run project
+```
+pip3 install -r requirements.txt
+```
+- Download [ngrok](https://ngrok.com/download)
+- Login to ngrok from your browser.
+- Follow instructions [here](https://dashboard.ngrok.com/get-started/setup) to connect to your account from your local machine.
+
+### Facebook App Setup
+- Login/Register to [Facebook Developers](https://developers.facebook.com/) site
+- Create a new app
+![create](imgs/create_app.png)
+- Select Business app type then Click *Continue*
+![select](imgs/select_type.png)
+- Enter app name and Contact email Then click *Create APP*
+![select](imgs/app_config.png)
+- Select the Product *Messenger* and Click *SetUp*
+![product](imgs/add_product.png)
+- Create new facebook page or Add an Existing Page where you will use the bot.
+![page](imgs/create_page.png)
+- Generate an Access Token for the page. Copy and keep this token secrete and do not share with any one.
+![token](imgs/generate_token.png)
+
+## Running the App
+- We would clone this repo
+- Navigate to the project root folder
+- create a *.env* file with fields
+    - -> VERIFIER_JETON=`"add any verificatio message here"`
+    - -> PAGE_ACCESS_TOKEN=`"add the token generated from facebook here"`
+
+- Now run the flask app
+```
+FLASK_APP=mybot.py flask run
+```
+![flask](imgs/flask_app.png)
+- Navigate to where you downloaded the *ngrok* application and run ngrok.
+```
+./ngrock http 5000
+```
+![ngrok](imgs/ngrok.png)
+- Copy the Fowarding url with https
+```
+https://230696fa7855.ngrok.io
+```
+- Go back to your facebook app settings and Edit the Webhook
+![webhook](imgs/webhook.png)
+- Insert the url you coppied adding `/webhook` to hit the webhook endpoint served by the app
+```
+https://230696fa7855.ngrok.io/webhook
+```
+![verify](imgs/verify_token.png)
+- Save the configurations. The webhook config page should close without errors.
+- You should see a message from your flask app terminal showing your conncetion was successfull.
+![verified](imgs/verified.png)
+- Now send a message to your faceboog page and it should respond to you directly.
--- mybot.py
+++ mybot.py
@@ -1,33 +1,52 @@
 from flask import Flask, request
-
+from dotenv import load_dotenv
+import os
 from pymessenger import Bot
 app = Flask(__name__)
 
-VERIFY_TOKEN = 'some-verify-token'# <paste your verify token here>
-PAGE_ACCESS_TOKEN = 'EAALQwNvlnS8BAGtpmj2un7fHGQlWrbVFEvvPuRvr8EP1ZB7i4A7ze1n6U4VHOF2lJK99eazVO3846Y3UjiSEXp6ZBNhmZCsJDUL0YrROHGdfyF6pZB8rNecsn8mPpqh061zOI2hQy0ZAG7ubG3dQ185iHMx21b8J7yDZCyX0w1wQZDZD'
+load_dotenv()
+VERIFIER_JETON = os.environ.get('VERIFIER_JETON')
+PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 FB_API_URL = 'https://graph.facebook.com/v10.0/me/messages?access_token='+PAGE_ACCESS_TOKEN
 
+# make connection to graph api
 bot = Bot(PAGE_ACCESS_TOKEN)
 
+
 def verify_webhook(req):
-    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
+    """Cette fonction permet de vérifier que le jeton passé correspond à celui sur le webhook
+         paramètres:
+             req: la requête envoyée au bot de messagerie
+         revenir:
+             renvoie une chaîne indiquant le succès de la connexion, sinon le jeton est incorrect.
+    """
+    if req.args.get("hub.verify_token") == VERIFIER_JETON:
         return req.args.get("hub.challenge")
     else:
-        return "incorrect"
+        return "incorrect token"
 
 def process_message(message):
+    """Cette fonction permet de traiter les messages envoyés à la page et la réponse du bot à l'expéditeur
+        paramètres:
+            message: le message envoyé par le client
+        revenir:
+            réponse: réponse envoyée par le bot au client
+    """
+
+    # convertir le message en minuscules pour un traitement facile
     formatted_message = message.lower()
+
     if formatted_message == "test":
-        response = "Test Sucessfull"
+        response = "Test réussi"
     else:
-        response = "Welcome to our page."
+        response = "Bienvenue sur notre page."
 
     return response
 
 @app.route("/webhook", methods=["POST", "GET"])
 def webhook():
-    """This is the main function flask uses to 
-    listen at the `/webhook` endpoint"""
+    """C'est la fonction principale utilisée par flask pour
+     écouter au point de terminaison `/ webhook`"""
     if request.method == 'GET':
         return verify_webhook(request)
     elif request.method == "POST":
@@ -41,7 +60,7 @@ def webhook():
             response = process_message(text)
             bot.send_text_message(sender_id, response)
 
-            return "Message received."
+            return "Message reçu."
 
         else:
             return "200"
